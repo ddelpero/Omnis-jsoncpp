53aedb22df8ec35047d92b3505327d1557619716
ddelpero <ddelpero@gmail.com>
2014-08-11 01:25:56 -0600

    batch file to set environment variables for Studio 4.3.2.

---
+++ proj/Windows/set_vars.4.3.2.bat
@@ -0,0 +1,8 @@
+set VERSION=4.3.2
+set STUDIO_FOLDER=Studio43.dev
+setx OMNIS_LIB_PATH "%CD%\EXTCOMP.%VERSION%
+setx BOOST_ROOT "C:\Program Files\boost\boost_1_51"
+setx OMNIS_XCOMP_PATH "C:\Program Files\RainingData\%STUDIO_FOLDER%\xcomp"
+
+
+pause
